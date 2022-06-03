8fa9b0000cd71f3a5306e71cf68c0f357cebcbea
Igor Busarov <igobusarov@yahoo.com>
2022-06-03 23:18:27 +0300

    Set project structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 
 const PORT = 5000
 
 const app = express()
 
+app.use('/static',express.static('public'))
+
+app.engine(
+  'hbs',
+  handlebars.engine({
+    extname: 'hbs',
+  })
+)
+app.set('view engine', 'hbs')
+app.set('views', './src/views')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
