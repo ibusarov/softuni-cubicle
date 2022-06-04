d2aa31ade6055842662bb295bb8f7d66b0c77aa2
Igor Busarov <igobusarov@yahoo.com>
2022-06-04 20:47:12 +0300

    Add id to db

--- src/services/cubeServices.js
+++ src/services/cubeServices.js
@@ -2,11 +2,10 @@ const fs = require('fs/promises')
 const path = require('path')
 const cubes = require('../db.json')
 
-
 exports.getOne = (cubeId) => cubes[cubeId]
 
 exports.save = (cube) => {
-  cubes.push(cube)
+  cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube })
 
   let textData = JSON.stringify(cubes, '', 4)
 
