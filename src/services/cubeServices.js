1d68c0d5a0321b2d8db6367f9c409c68226c12c4
Igor Busarov <igobusarov@yahoo.com>
2022-06-04 22:25:49 +0300

    id fixed to number

--- src/services/cubeServices.js
+++ src/services/cubeServices.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises')
 const path = require('path')
 const cubes = require('../db.json')
 
-exports.getOne = (cubeId) => cubes[cubeId]
+exports.getOne = (cubeId) => cubes.find((x) => x.id === Number(cubeId))
 
 exports.getAll = (serach = '', fromInput, toInput) => {
   const from = Number(fromInput) || 0
