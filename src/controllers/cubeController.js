f158aec99ccff1c29f220e87911466ba181f07d4
Igor Busarov <igobusarov@yahoo.com>
2022-06-04 20:05:38 +0300

    cubuc service added

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,33 +1,26 @@
 const router = require('express').Router()
-const fs = require('fs/promises')
-const path = require('path')
-const cubes = require('../db.json')
+const cubeServices = require('../services/cubeServices')
 
 router.get('/create', (req, res) => {
   res.render('create')
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   const cube = req.body
 
   //Validate
-
   if (cube.name.length < 2) {
-    res.status(400).send('Invalod request')
+    res.status(400).send('Invalid request')
     return
   }
 
   //Save data
-
-  cubes.push(cube)
-
-  fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {
-    encoding: 'utf-8',
-  })
-    .then(() => {
-      res.redirect('/')
-    })
-    .catch((err) => res.status(400).send(err))
+  try {
+    await cubeServices.save(cube)
+    res.redirect('/')
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 module.exports = router
