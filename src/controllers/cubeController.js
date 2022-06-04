3e1dfa33358b1dd44c1cb60d864a9524a62504ff
Igor Busarov <igobusarov@yahoo.com>
2022-06-04 19:46:39 +0300

    Post cubic

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,33 @@
 const router = require('express').Router()
+const fs = require('fs/promises')
+const path = require('path')
+const cubes = require('../db.json')
 
 router.get('/create', (req, res) => {
-    res.render('create')
+  res.render('create')
+})
+
+router.post('/create', (req, res) => {
+  const cube = req.body
+
+  //Validate
+
+  if (cube.name.length < 2) {
+    res.status(400).send('Invalod request')
+    return
+  }
+
+  //Save data
+
+  cubes.push(cube)
+
+  fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {
+    encoding: 'utf-8',
+  })
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch((err) => res.status(400).send(err))
 })
 
 module.exports = router
