ef4323d94b88a037112b66460657c9bc243e98fe
Igor Busarov <igobusarov@yahoo.com>
2022-06-12 19:22:27 +0300

    Show accessories in the detailed page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -26,6 +26,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/details/:id', async (req, res) => {
   const cube = await cubeServices.getOne(req.params.id).lean()
+  // console.log(cube);
   res.render('details', { cube })
 })
 
--- src/services/cubeServices.js
+++ src/services/cubeServices.js
@@ -5,7 +5,16 @@ const Accessory = require('../models/Accessory')
 
 const Cube = require('../models/Cube')
 
-exports.getOne = (cubeId) => Cube.findById(cubeId)
+exports.getOne = (cubeId) => Cube.findById(cubeId).populate('accessories')
+
+//Nested population
+// .popiulate({
+//   path: 'accessories',
+//   populate: {
+//     path: 'cubes',
+//     model: 'Cube'
+//   }
+// })
 
 exports.getAll = async (serach = '', fromInput, toInput) => {
   let cubes = await Cube.find().lean()
