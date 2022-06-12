d082180abc825426de17e35d4e282cfa42ae4257
Igor Busarov <igobusarov@yahoo.com>
2022-06-12 20:28:17 +0300

    Search filters

--- src/services/cubeServices.js
+++ src/services/cubeServices.js
@@ -6,7 +6,8 @@ const Accessory = require('../models/Accessory')
 const Cube = require('../models/Cube')
 
 exports.getOne = (cubeId) => Cube.findById(cubeId)
-exports.getOneDetails = (cubeId) => Cube.findById(cubeId).populate('accessories')
+exports.getOneDetails = (cubeId) =>
+  Cube.findById(cubeId).populate('accessories')
 
 //Nested population
 // .popiulate({
@@ -17,8 +18,16 @@ exports.getOneDetails = (cubeId) => Cube.findById(cubeId).populate('accessories'
 //   }
 // })
 
-exports.getAll = async (serach = '', fromInput, toInput) => {
-  let cubes = await Cube.find().lean()
+exports.getAll = async (search = '', fromInput, toInput) => {
+  // let cubes = await Cube.find().lean()
+  const from = Number(fromInput) || 0
+  const to = Number(toInput) || 6
+
+  let cubes = await Cube.find({ name: { $regex: new RegExp(search, 'i') } })
+    .where('difficultyLevel')
+    .lte(to)
+    .gte(from)
+    .lean()
 
   return cubes
   // const from = Number(fromInput) || 0
